e795b159bfc7276a23759632e0a815617b9d0156
Custom Software <kcluff50@gmail.com>
2024-02-16 07:49:19 -0700

    New formulas

--- Sources/NavTool/NavTool.swift
+++ Sources/NavTool/NavTool.swift
@@ -10,4 +10,13 @@ class NavTool {
     
     public let appName: String = "NavTool"
     public let version: String = "0.1"
+    
+    
+    public func convertToRadians(_ degrees: Double) -> Double {
+        return degrees * (Double.pi/180)
+    }
+    
+    public func convertToDegrees(_ radians: Double) -> Double {
+        return radians * 180 / Double.pi
+    }
 }
