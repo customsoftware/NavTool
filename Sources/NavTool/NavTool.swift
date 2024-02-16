222e02a793eb0703ba7673c96845476e2b7ee405
Custom Software <kcluff50@gmail.com>
2024-02-16 07:37:19 -0700

    Incorporate local package

--- Sources/NavTool/NavTool.swift
+++ Sources/NavTool/NavTool.swift
@@ -1,2 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+import Foundation
+
+public
+class NavTool {
+    public static let shared: NavTool = NavTool()
+    
+    public let appName: String = "NavTool"
+    public let version: String = "0.1"
+}
--- Tests/NavToolTests/NavToolTests.swift
+++ Tests/NavToolTests/NavToolTests.swift
@@ -2,10 +2,17 @@ import XCTest
 @testable import NavTool
 
 final class NavToolTests: XCTestCase {
+    
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
-
+        let nameVal = NavTool.shared.appName
+        XCTAssertTrue(nameVal == "NavTool", "It should be 'NavTool', but is '\(nameVal)'")
+        
+        let toolVersion = NavTool.shared.version
+        XCTAssertTrue(toolVersion == "0.1", "Version should be '0.1', but is '\(toolVersion)'")
+        
+        
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
